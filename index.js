d6582b0f93f1258ee25d487278b72aa8961730f2
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-04-04 00:49:33 +0530

    HEY RAM

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static( __dirname + '/uploads'));
 app.use(cors
     ({
-        origin: "https://ankit-hotel.vercel.app/",
+        origin: "*",
         credentials: true
     }));
 
